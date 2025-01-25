d87849095147a1d6e5ac154f1a1cd29b0ecd87a9
carmenscar <carmenscartezini@gmail.com>
2025-01-25 20:11:54 -0300

    fixing basic_cleaning correcting latitute and longitude

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -33,6 +33,10 @@ def go(args):
     logging.info("Transforming column in date format")
     df['last_review'] = pd.to_datetime(df['last_review'])
 
+    logging.info("adjusting latitude and longitude")
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
     logger.info("Saving df: 'clean_sample.csv'...")
     df.to_csv("clean_sample.csv", index=False)
 
